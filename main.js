c625b7bcf1f46768a56fc4e3bf5878842eaf4d3b
Forrest <fmilner@zoom456.com>
2021-05-11 13:09:45 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,90 @@
+const { app, BrowserWindow } = require('electron')
+
+if (require('electron-squirrel-startup')) return app.quit();
+
+const SerialPort = require('serialport');
+const { ipcMain } = require('electron')
+const Readline = require('@serialport/parser-readline');
+const fs = require('fs');
+
+const send_fake_data = true;
+
+let port;
+function createPort(portID) {
+    if (send_fake_data) { const string = fs.readFileSync('data.json'); setInterval(() => parseAndSend(string), 2100); } else {
+        while (!port) {
+            port = new SerialPort(portID, { baudRate: 115200 }, (err) => {
+                if (err) return console.log('Error: ', err.message)
+                else return console.log('Successfully opened port.')
+            });
+        }
+        const parser = port.pipe(new Readline({ delimiter: '\n' }));
+        parser.on('data', data => {
+            parseAndSend(data);
+        });
+    }
+}
+
+ipcMain.on('ready_for_data', () => {
+    console.log('The program is ready for data');
+    SerialPort.list().then(ports => {
+        // if (err) return sendError('Failed to get list of available ports');
+        portsList = []
+        ports.forEach((port) => {
+            portsList.push(port.path);
+        });
+        if (portsList.length == 1) {
+            console.log('Found and using port ' + portsList[0])
+            createPort(portsList[0])
+        } else {
+            //Ask the user what to do now by sending a request.
+            if (portsList.length == 0) portsList = 'None'
+            console.log('Ports found: ' + portsList + '. Asking user to select port.')
+            win.webContents.send('select_port', portsList)
+        }
+    }).catch(console.log)
+});
+
+ipcMain.on('selected_port', (event, port) => {
+    console.log('The user has selected port ' + port)
+    createPort(port);
+});
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 1280,
+        height: 800,
+        // fullscreen: true,
+    })
+
+    win.loadFile('index.html')
+}
+
+function parseAndSend(string) {
+    let jsonData;
+    try {
+        jsonData = JSON.parse(string);
+    } catch (error) {
+        console.log(string)
+    }
+    win.webContents.send('data', jsonData)
+}
+
+function sendError(err) {
+    console.log("Error: " + err);
+    win.webContents.send('error', err)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        console.log("Closing program.")
+        app.quit()
+    }
+})
---
+++ script.js
@@ -0,0 +1,49 @@
+let data;
+let lastUpdateDate;
+var fake_data = typeof require === 'undefined';
+console.log(fake_data ? "Faking data.." : "Not faking data.")
+const do_title = false;
+const do_time_based_themes = false;
+
+Math.clamp = function (number, min, max) {
+    return Math.max(min, Math.min(number, max));
+}
+
+Array.prototype.last = function () {
+    return this[this.length - 1];
+}
+
+Math.map = function (in_min, in_max, out_min, out_max, val) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+if (!fake_data) {
+    const { ipcRenderer, Renderer } = require('electron')
+
+    ipcRenderer.on('data', (event, _data) => {
+        // console.log(_data.s)
+        if (_data.s == "normal") {
+            data = _data;
+            $('#status').html("Got last data " + new Date().toLocaleTimeString())
+            $('#status').css({ "background-color": "grey" })
+            lastUpdateDate = new Date();
+            onUpdate();
+        } else if (_data.s == "bms_error") {
+            console.log("BMS Error: " + _data.error)
+        } else if (_data.s == "log") {
+            console.log(_data.m)
+        }
+    })
+
+    ipcRenderer.on('select_port', (event, _data) => {
+        $('#overlay').show();
+    })
+}
+
+
+
+function sendPort() {
+    console.log('Sending port ' + $('#portsSelect').val())
+    ipcRenderer.send('selected_port', $('#portsSelect').val())
+    $('#overlay').hide();
+}
