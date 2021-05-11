3a946dcbc0bc66ce4bb2ba18c0375a0d1e23c52e
Forrest <fmilner@zoom456.com>
2021-05-11 16:49:26 -0400

    Automated Push after make

--- buildAndPush.bat
+++ buildAndPush.bat
@@ -1,5 +1,10 @@
+ECHO "Starting build"
+CALL npm run make
+ECHO "Finished build"
+PAUSE
 git add .
-git commit
-git push origin
-ECHO Yeet
+git commit -m "Automated Push after make"
+git push
+ECHO "Finished Push"
+ECHO "IT WORKED"
 PAUSE
